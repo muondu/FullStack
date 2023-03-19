6604de1c4e66187a1f7f0fb3592f2638c7ab1308
muondu <munenemuondu@gmail.com>
2023-03-19 12:29:13 +0300

    Differentiating between const, let & var

--- JAVASCRIPT/Debugging external files/script.js
+++ JAVASCRIPT/Debugging external files/script.js
@@ -6,3 +6,17 @@ var studentName = "NeroNesh";
 
 console.log("My name is:",studentName)
 console.log(beast)
+
+// Const and let variables
+// Difference is that the data in let can be changed while data in const can't be changed
+
+if (studentName == "NeroNesh"){
+    const pens = 3;
+    let apples = 2;
+    var pencil = 1;
+    const height = "5'8\"" ; 
+    console.log("I have", apples, "apples and", pens, "pens and ",pencil,"pencil");
+ 
+}
+// Var is universal thus can be used anywhere while let and const can't be used anywhere(if inside a function)
+console.log("I have", pencil,"pencil");
