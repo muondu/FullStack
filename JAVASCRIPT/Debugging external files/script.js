47be9b3d8f430ff61a0535a4a24f9f7bc9939495
muondu <munenemuondu@gmail.com>
2023-03-19 12:00:19 +0300

    Stored data in variables

--- JAVASCRIPT/Debugging external files/script.js
+++ JAVASCRIPT/Debugging external files/script.js
@@ -1,2 +1,8 @@
 console.log("Page successfully loaded in from a another site.")
-// 
+// Storing Data in variables
+
+var beast = "https://www.udemy.com/course/the-ultimate-fullstack-web-development-bootcamp/learn/lecture/23513644#overview";
+var studentName = "NeroNesh";
+
+console.log("My name is:",studentName)
+console.log(beast)
