17af29998aed8b9498498a2699699ba6d74221b0
muondu <munenemuondu@gmail.com>
2023-03-20 12:13:24 +0300

    Have done conditional statement

---
+++ JAVASCRIPT/Conditional Statement/script.js
@@ -0,0 +1,21 @@
+const beast = "Nesh OOF";
+
+if(beast == "Nesh"){
+    // Executes code if true
+    console.log("My name is ",beast)
+} else{
+    // Executes code if false
+    // alert("Error");
+    console.error("Error")
+}
+
+
+const age = 18;
+
+if (age > 18) {
+    console.log("You can drink alcohol")
+} else if(age == 18){
+    console.log("Yay")
+}else{
+    console.log("Too young")
+}
