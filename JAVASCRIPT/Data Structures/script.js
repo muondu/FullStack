91780d17a5bc2796f35157360aa78ed8ce106e09
muondu <munenemuondu@gmail.com>
2023-03-20 11:15:15 +0300

    Have done data structures

---
+++ JAVASCRIPT/Data Structures/script.js
@@ -0,0 +1,12 @@
+const str = "string";
+const num = 69;
+const float = 3.14; // Decimal
+const bool = true; // or false
+const list = ["Item 1",2,3.84,[],false];// Array = List
+const thing = null; // Has nothing in it
+const notDefined = undefined; // Currently holds no value
+const getAge = function(){ return 69; }
+const object = {
+    'name':'Nesh',
+    'sport':"Tennis",
+} // Object can be accessed with obj['name']
