f1f76b6a669e6b34899248ee4e7934ddc9776b81
muondu <munenemuondu@gmail.com>
2023-03-20 10:36:21 +0300

    Have added operators

---
+++ JAVASCRIPT/Operators/operators.js
@@ -0,0 +1,9 @@
+// + is addition
+// - is subtraction
+// * is multiplication
+// / is division
+// ** is square a number
+// % gets the remainder eg 10 % 3 = 1 because the remainder will be 1
+// total++ does it that it going to return the same value but when you call it again it would have added +1
+// ++total does is that its going to return add the number then return it
+// total-- does it that it going to return the same value but when you call it again it would have subtracted -1
